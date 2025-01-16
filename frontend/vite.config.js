efcaf02e3a76c408c3fd52ed02161b73249c159c
solaris-9 <solaris-9@githube.com>
2025-01-16 04:30:22 +0200

    ready to publish

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -47,6 +47,7 @@ export default defineConfig(({ command, mode }) => {
       preprocessorOptions: {
         scss: {
           implementation: require("sass"),
+          api: 'modern-compiler'
         },
       },
     }
